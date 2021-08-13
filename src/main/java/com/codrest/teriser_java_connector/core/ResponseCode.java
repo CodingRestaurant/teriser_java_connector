ce6f28158c8c7db19e6f8bed128da8027d75f70f
Arakene <arakene_dev@naver.com>
2021-08-13 17:23:29 +0900

    Add responseCode

---
+++ src/main/java/com/codrest/teriser_java_connector/core/ResponseCode.java
@@ -0,0 +1,10 @@
+package com.codrest.teriser_java_connector.core;
+
+public class ResponseCode {
+
+    public static final String OK = "200";
+    public static final String NotFound = "404";
+    public static final String NODevelopID = "403";
+    public static final String NullValue = "412";
+    public static final String ServerError = "500";
+}
