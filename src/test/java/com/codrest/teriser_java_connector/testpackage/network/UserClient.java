d31dee50daa5485a881122be2ccee1ce5611e78c
Arakene <arakene_dev@naver.com>
2021-08-13 22:57:33 +0900

    Update UserClient

--- src/test/java/com/codrest/teriser_java_connector/testpackage/network/UserClient.java
+++ src/test/java/com/codrest/teriser_java_connector/testpackage/network/UserClient.java
@@ -8,11 +8,9 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import javax.net.SocketFactory;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
@@ -20,7 +18,7 @@ import java.util.concurrent.Executors;
 
 public class UserClient {
 
-    private Socket client;
+    private SocketChannel client;
     private ExecutorService executors;
 
     public UserClient() {
@@ -30,40 +28,46 @@ public class UserClient {
 
     private void connectServer() {
         try {
-            client = SocketFactory.getDefault().createSocket("127.0.0.1", 10101);
+            client = SocketChannel.open();
+            client.connect(new InetSocketAddress("127.0.0.1", 10101));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void sendData(){
-        JsonObject test = new JsonObject();
+    public void sendData() {
+        executors.execute(() -> {
+            JsonObject test = new JsonObject();
 
-        CubeData cubeData = new CubeData();
-        cubeData.setName("cube");
+            CubeData cubeData = new CubeData();
+            cubeData.setName("cube");
 
-        User user = new User();
-        user.setName("User Name");
+            User user = new User();
+            user.setName("User Name");
 
-        Gson gson = new GsonBuilder().create();
+            Gson gson = new GsonBuilder().create();
 
-        test.add("CubeData", JsonParser.parseString(gson.toJson(cubeData)));
-        test.add("User", JsonParser.parseString(gson.toJson(user)));
+            test.add("CubeData", JsonParser.parseString(gson.toJson(cubeData)));
+            test.add("User", JsonParser.parseString(gson.toJson(user)));
 
-        DataPacketBuilder builder = new DataPacketBuilder("developer ID", "Project ID", 1);
+            DataPacketBuilder builder = new DataPacketBuilder("developer ID", "Project ID", 1);
 
-        String msg = builder
-                .setMethodName("helloWorld")
-                .setMethodParameter(test)
-                .buildClientMessage();
+            String msg = builder
+                    .setMethodName("helloWorld")
+                    .setMethodParameter(test)
+                    .buildClientMessage();
 
-        try {
-            OutputStream stream = client.getOutputStream();
-            stream.write(msg.getBytes(StandardCharsets.UTF_8));
-            stream.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            try {
+                byte[] data = msg.getBytes(StandardCharsets.UTF_8);
+                ByteBuffer buffer = ByteBuffer.allocate(data.length + 4);
+                buffer.putInt(data.length);
+                buffer.put(data);
+
+                client.write(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 }
