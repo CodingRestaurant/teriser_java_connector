9671e45f8ff385441188a8d235d1c528058dc394
Arakene <arakene_dev@naver.com>
2021-08-13 16:52:19 +0900

    Update serverMessage

--- src/main/java/com/codrest/teriser_java_connector/core/ServerMessage.java
+++ src/main/java/com/codrest/teriser_java_connector/core/ServerMessage.java
@@ -6,40 +6,40 @@ import java.util.Arrays;
  * Server to Client Message
  *
  * Need 4 parameters
- * requestCode -> Identify message (use same requestCode in client message)
- * statusCode -> result Code (follow http status code)
+ * messageID -> Identify message (use same requestCode in client message)
+ * responseCode -> result Code (follow http status code)
  * data -> requested method result
  * errorMessage -> Error log or null
  */
 public class ServerMessage {
 
-    private int requestCode;
-    private String statusCode;
+    private int messageID;
+    private String responseCode;
     private String[] data;
     private String errorMessage;
 
     /**
      * Server Error Message
-     * @param requestCode Identify message (use same requestCode in client message)
-     * @param statusCode result Code (follow http status code)
+     * @param messageID Identify message (use same requestCode in client message)
+     * @param responseCode result Code (follow http status code)
      * @param errorMessage Error log
      */
-    public ServerMessage(int requestCode, String statusCode, String errorMessage) {
-        this.requestCode = requestCode;
-        this.statusCode = statusCode;
+    public ServerMessage(int messageID, String responseCode, String errorMessage) {
+        this.messageID = messageID;
+        this.responseCode = responseCode;
         this.errorMessage = errorMessage;
         data = null;
     }
 
     /**
      * Server Success Message
-     * @param requestCode Identify message (use same requestCode in client message)
-     * @param statusCode result Code (follow http status code)
+     * @param messageID Identify message (use same requestCode in client message)
+     * @param responseCode result Code (follow http status code)
      * @param data requested method result
      */
-    public ServerMessage(int requestCode, String statusCode, String[] data) {
-        this.requestCode = requestCode;
-        this.statusCode = statusCode;
+    public ServerMessage(int messageID, String responseCode, String[] data) {
+        this.messageID = messageID;
+        this.responseCode = responseCode;
         this.data = data;
         errorMessage = null;
     }
@@ -47,8 +47,8 @@ public class ServerMessage {
     @Override
     public String toString() {
         return "ServerMessage{" +
-                "requestCode=" + requestCode +
-                ", statusCode='" + statusCode + '\'' +
+                "messageID=" + messageID +
+                ", responseCode='" + responseCode + '\'' +
                 ", data=" + Arrays.toString(data) +
                 ", errorMessage='" + errorMessage + '\'' +
                 '}';
