5c7a834d7883d2bbcb7cc5b6b913c4335dbac130
Arakene <arakene_dev@naver.com>
2021-08-17 19:40:17 +0900

    Update http server sneding  response

--- src/test/java/com/codrest/teriser_java_connector/testpackage/network/TeriserClient.java
+++ src/test/java/com/codrest/teriser_java_connector/testpackage/network/TeriserClient.java
@@ -6,19 +6,19 @@ import com.codrest.teriser_java_connector.core.net.MessageReceiver;
 import com.codrest.teriser_java_connector.testpackage.TestBot;
 import com.codrest.teriser_java_connector.testpackage.TestBot2;
 import com.codrest.teriser_java_connector.testpackage.TestBot3;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -46,17 +46,29 @@ public class TeriserClient {
 
                     StringBuilder builder = new StringBuilder();
 
-                    try{
+                    try {
                         String line = "";
                         while ((line = reader.readLine()) != null) {
                             builder.append(line);
                         }
-                    }
-                    catch (IOException e1){
+                    } catch (IOException e1) {
                         e1.printStackTrace();
                     }
 
-                    teriser.request(builder.toString());
+                    String result = teriser.request(builder.toString());
+
+                    Headers headers = exchange.getResponseHeaders();
+                    headers.add("Content-Type", "application/json");
+                    headers.add("Content-Length", String.valueOf(result));
+
+                    OutputStream stream = exchange.getResponseBody();
+                    try {
+                        exchange.sendResponseHeaders(200, result.length());
+                        stream.write(result.getBytes(StandardCharsets.UTF_8));
+                        stream.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             });
 
